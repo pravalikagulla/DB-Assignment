b105567fe42721064501886ec0d70764359a99a5
Gulla pravalika <107388693+pravalikagulla@users.noreply.github.com>
2024-03-11 15:56:37 +0530

    schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,46 @@
+Table product_category {
+  id int [primary key]
+  name varchar
+  desc text
+  created_at timestamp
+  modified_at timestamp
+  deleted_at timestamp
+}
+Table product_inventory{
+  id int [primary key]
+  quantity int
+  created_at timestamp
+  modified_at timestamp
+  deleted_at timestamp
+}
+Table discount{
+  id int [primary key]
+  name varchar
+  desc text
+  discount_percent decimal
+  active boolean
+  created_at timestamp
+  modified_at timestamp
+  deleted_at timestamp
+}
+
+Table product{
+  id int [primary key]
+  name varchar
+  desc text
+  SKU varchar
+  category_id int [ref:> product_category.id]
+  inventory_id int [ref:> product_inventory.id]
+  price decimal
+  discount_id int [ref:> discount.id]
+  created_at timestamp
+  modified_at timestamp
+  deleted_at timestamp
+}
+
+
+
+
+
+
 
